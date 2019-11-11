547e4774fa4ec1b358a5fec6ed0d21d97ffbf1d4
Pierre-Adrien Delisle <pierre-adrien.delisle@etu.univ-nantes.fr>
2019-11-11 19:53:14 +0100

    launch experiment

--- Scripts/REST-API.py
+++ Scripts/REST-API.py
@@ -13,20 +13,20 @@ rGetTotal = requests.get(apiServer+"/experiments/total", auth=myAuth)
 res = json.loads((rGetTotal.text))
 
 
-
-headers = {"content-type":"multipart/form-data"}
-submitData =  json.dumps({"experiment":
-                {"type":"alias",
-                 "duration":2,
-                 "name":"ApiRestPython",
-                 "nodes":[{"alias":1,"nbnodes":3,"properties":{"archi":"m3:at86rf231","site":"lille","mobile":"false"}}],
-                 "firmwareassociations":None,
-                 "profileassociations":None,
-                 }
-                })
-rSubmit = requests.post(apiServer+"/experiments/",data=submitData, auth=myAuth,headers=headers)
+    
+submitData = {"experiment": json.dumps({"type":"alias",
+             "duration":20,
+             "name":"ApiRestPython",
+             "nodes":[{"alias":1,"nbnodes":3,"properties":{"archi":"m3:at86rf231","site":"lille","mobile":"false"}}],
+             "firmwareassociations":[{"nodes":[1],"firmwarename":"iotlab_m3_tutorial"}],
+             "profileassociations":None,
+             })
+            }
+rSubmit = requests.post(apiServer+"/experiments",files=submitData, auth=myAuth)
 
 resSubmit = rSubmit.text
 print(resSubmit)
+exp_id = json.loads((rSubmit.text))["id"]
 
 #experiment.json: {"type":"alias","duration":20,"name":"","nodes":[{"alias":1,"nbnodes":3,"properties":{"archi":"m3:at86rf231","site":"lille","mobile":false}}],"firmwareassociations":null,"profileassociations":null}
+
