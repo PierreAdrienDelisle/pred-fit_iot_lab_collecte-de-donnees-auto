2d99a42d92d176a150a715cf4731d6a54533431e
Pierre-Adrien Delisle <pierre-adrien.delisle@etu.univ-nantes.fr>
2019-11-11 11:58:54 +0100

    Test REST Api

--- Scripts/REST-API.py
+++ Scripts/REST-API.py
@@ -2,5 +2,31 @@
 
 ### REST-API : Tools to communicate in REST
 import requests
-r = requests.get('https://github.com/timeline.json')
-print(r.text)
+from requests.auth import HTTPBasicAuth
+import json
+
+myAuth = HTTPBasicAuth('user', 'pass')
+
+apiServer = "https://www.iot-lab.info/api"
+
+rGetTotal = requests.get(apiServer+"/experiments/total", auth=myAuth)
+res = json.loads((rGetTotal.text))
+
+
+
+headers = {"content-type":"multipart/form-data"}
+submitData =  json.dumps({"experiment":
+                {"type":"alias",
+                 "duration":2,
+                 "name":"ApiRestPython",
+                 "nodes":[{"alias":1,"nbnodes":3,"properties":{"archi":"m3:at86rf231","site":"lille","mobile":"false"}}],
+                 "firmwareassociations":None,
+                 "profileassociations":None,
+                 }
+                })
+rSubmit = requests.post(apiServer+"/experiments/",data=submitData, auth=myAuth,headers=headers)
+
+resSubmit = rSubmit.text
+print(resSubmit)
+
+#experiment.json: {"type":"alias","duration":20,"name":"","nodes":[{"alias":1,"nbnodes":3,"properties":{"archi":"m3:at86rf231","site":"lille","mobile":false}}],"firmwareassociations":null,"profileassociations":null}
